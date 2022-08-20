0007a6e7312e229df6a7738e48d064b2908eb65b
Kaique-Andrade <80967058+Kaique-Andrade@users.noreply.github.com>
2022-08-20 12:59:00 -0300

    apostila06: fim da apostila

--- src/app/clientes/cliente-inserir.component.ts
+++ src/app/clientes/cliente-inserir.component.ts
@@ -1,5 +1,8 @@
+import { OnInit } from '@angular/core';
 import { Component} from '@angular/core'
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Cliente } from './cliente.model';
 import { ClienteService } from './cliente.service';
 @Component({
   selector:'app-cliente-inserir',
@@ -7,18 +10,55 @@ import { ClienteService } from './cliente.service';
   styleUrls:['./cliente-inserir.component.css']
 })
 
-export class ClienteInserirComponent {
-  constructor(public clienteService: ClienteService) {}
+export class ClienteInserirComponent implements OnInit{
+  private modo: string = "criar"
+  private idCliente: string;
+  public cliente: Cliente;
+  constructor(
+    public clienteService: ClienteService,
+    public route: ActivatedRoute
+    ) {}
 
-  onAdicionarCliente(form: NgForm){
+  ngOnInit(): void{
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('idCliente')){
+        this.modo = 'editar'
+        this.idCliente = paramMap.get('idCliente')
+        this.clienteService.getCliente(this.idCliente).subscribe (dadosCli => {
+          this.cliente = {
+            id: dadosCli._id,
+            nome: dadosCli.nome,
+            fone: dadosCli.fone,
+            email: dadosCli.email
+          }
+        })
+      }
+      else{
+        this.modo = 'criar'
+        this.idCliente = null
+      }
+    })
+  }
+
+  onSalvarCliente(form: NgForm){
     if (form.invalid){
       return;
     }
-    this.clienteService.adicionarCliente(
-      form.value.nome,
-      form.value.fone,
-      form.value.email
-    );
+    if (this.modo === "criar"){
+      this.clienteService.adicionarCliente(
+        form.value.nome,
+        form.value.fone,
+        form.value.email
+      );
+    }
+    else{
+      this.clienteService.atualizarCliente(
+        this.idCliente,
+        form.value.nome,
+        form.value.fone,
+        form.value.email
+      )
+    }
     form.resetForm();
   }
 }
